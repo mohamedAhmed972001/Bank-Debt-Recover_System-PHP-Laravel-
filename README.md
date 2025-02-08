f43c4f976c60bcea3ec265c090c5d15c635ae36f
mohamedAhmed972001 <mezo1927061@gmail.com>
2025-02-08 15:48:53 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Bank-Debt-Recover_System-PHP-Laravel-
-Bank-Debt-Recover_System(PHP Laravel))
--- resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Footer opened -->
-	<div class="main-footer ht-40">
-		<div class="container-fluid pd-t-0-f ht-100p">
-			<span>Copyright © Developed by <a href="https://www.linkedin.com/in/mohamed-ahmed-b13502225/">Mohamed Ahmed</a> All rights reserved</span>
-		</div>
-	</div>
-<!-- Footer closed -->
